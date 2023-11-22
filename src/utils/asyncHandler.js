a7e9a4bc59ea96b242cab44e86638010f138cb4a
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-11-22 21:41:26 +0600

    wrapper function

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((error)=>next(error));
+    }
+}
+
+export {asyncHandler};
+
+
+//asyncHandler is a higher order function that takes a function as an argument and returns a function.
+/*
+const asyncHandler=(fn) =>async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message || 'Internal Server Error'
+        })
+    }
+}*/
